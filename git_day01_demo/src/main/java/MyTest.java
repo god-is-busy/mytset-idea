b5bea59db3c87a0583822373ba689d6aa820b2b9
hua.ren <734432053@qq.com>
2018-11-19 21:22:54 +0800

    11

--- git_day01_demo/src/main/java/MyTest.java
+++ git_day01_demo/src/main/java/MyTest.java
@@ -2,4 +2,46 @@ public class MyTest {
     private Integer id;
     private String name;
     private Integer age;
+    private String address;
+    private String adfa;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAdfa() {
+        return adfa;
+    }
+
+    public void setAdfa(String adfa) {
+        this.adfa = adfa;
+    }
 }
